5adce5806cc1f541f5be60bdb2599324a7141115
sushmabalikai <94390036+sushmabalikai@users.noreply.github.com>
2021-12-24 00:25:19 +0530

    Update Binary search tree.c

--- Binary search tree.c
+++ Binary search tree.c
@@ -1,169 +1,85 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 struct bst{
-    
-  int data;
-    
-  struct bst *left;
-    
-  struct bst *right;
-
+    int data;
+    struct bst *left;
+    struct bst *right;
 };
 
 struct bst *insert(struct bst *q,int val)
-
 {
-    
-  struct bst *temp;
-    
+    struct bst *temp;
     if(q==NULL)
-
-     {
-    
-      temp=(struct bst*)malloc(sizeof(struct bst));
-    
-      temp->data=val;
-    
-      temp->left=NULL;
-    
-      temp->right=NULL;
-    
-      q=temp;
-
-     }
-
-    else
-
-     {
-    
-      if(val<q->data)
-    
-       {
-        
-        q->left=insert(q->left,val);
-    
-       }
-    
+    {
+    temp=(struct bst*)malloc(sizeof(struct bst));
+    temp->data=val;
+    temp->left=NULL;
+    temp->right=NULL;
+    q=temp;
+    }
    else
-    
-     {
-        
-      q->right=insert(q->right,val);
-    
-     }
-
+   {
+    if(val<q->data)
+    {
+        q->left=insert(q->left,val);
+    }
+    else
+    {
+        q->right=insert(q->right,val);
+    }
 }
-
-  return q;
-
+    return q;
 }
 
 void inorder(struct bst *q)
-
 {
-    
-   if(q==NULL)
-    
+    if(q==NULL)
     {
-        
-     return;
-    
+         return;
     }
-    
-   inorder(q->left);
-    
-   printf("%d ",q->data);
-    
-   inorder(q->right);
-
+    inorder(q->left);
+    printf("%d ",q->data);
+    inorder(q->right);
 }
 
 struct bst *search(struct bst *p,int key,struct bst **y)
-
 {
-    
-  struct bst *temp;
-    
-  if(p==NULL)
-    
-  return(NULL);
-    
-  temp=p;
-    
-  *y=NULL;
-    
-  while(temp!=NULL)
-    
-  {
-        
-   if(temp->data==key)
-        
-   return(temp);
-        
-   else
-        
-   {
-            
-    *y=temp;
-            
-    if(temp->data>key)
-            
-    temp=temp->left;
-            
-    else
-            
-   temp=temp->right;
-        
-   }
-    
-   }
-    
-return(NULL);
-
+    struct bst *temp;
+    if(p==NULL)
+    return(NULL);
+    temp=p;
+    *y=NULL;
+    while(temp!=NULL)
+    {
+        if(temp->data==key)
+        return(temp);
+        else
+        {
+            *y=temp;
+             if(temp->data>key)
+            temp=temp->left;
+            else
+            temp=temp->right;
+        }
+    }
+    return(NULL);
 }
 
 int main()
-
 {
-    
-   struct bst *root;
-    
-   root=NULL;
-    
-   int n,val,num;
-    
-   printf("\n enter number of term:- ");
-    
-   scanf("%d",&n);
-    
-   while(n!=0)
-    
-   {
-       
-    printf("\n enter element:- ");
-       
-    scanf("%d",&val);
-        
-    root=insert(root,val);
-        
-    n--;
-    
+    struct bst *root;
+    root=NULL;
+    int n,val,num;
+    printf("\n enter number of term:- ");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+       printf("\n enter element:- ");
+       scanf("%d",&val);
+       root=insert(root,val);
+       n--;
     }
-   
    printf("\n display element:- ");
-    
    inorder(root);
-    
    return 1;
-
 }
-Output:
-enter number of term:- 5
-enter element:- 10
-enter element:- 7
-enter element:- 15
-enter element:- 9
-enter element:- 14
-display element:- 7 9 10 14 15 
