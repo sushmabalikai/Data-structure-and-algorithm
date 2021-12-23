feb21654fbaf87e852ba99e69f7cc050640358fd
sushmabalikai <94390036+sushmabalikai@users.noreply.github.com>
2021-12-24 00:30:21 +0530

    Add files via upload

---
+++ Binary tree.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+
+#include<stdlib.h>
+
+struct bt{
+    
+  int data;
+   
+  struct bt *left;
+    
+  struct bt *right;
+};
+struct  bt *insert(struct bt *q,int val,char dir)
+
+{
+    
+  struct bt*tmp;
+    
+  tmp=(struct bt *)malloc(sizeof(struct bt));
+    
+  tmp->data=val;
+    
+  tmp->left=tmp->right=NULL;
+    
+   if(dir=='l')
+    
+     q->left=tmp;
+    
+   else
+    
+     q->right=tmp;
+
+}
+
+void inorder(struct bt *q)
+
+{
+    
+  if(q==NULL)
+    
+   {
+        
+     return;
+    
+   }
+    
+   inorder(q->left);
+    
+   printf("%d ",q->data);
+    
+   inorder(q->right);
+    
+}
+    
+void preorder(struct bt *q)
+    
+{
+        
+  if(q!=NULL)
+        
+   {
+            
+     printf("%d ",q->data);
+            
+     preorder(q->left);
+           
+     preorder(q->right);
+            
+        
+   }
+    
+}
+        
+void postorder(struct bt *q)
+        
+{
+            
+  if(q!=NULL)
+            
+  {
+                
+    postorder(q->left);
+                
+    postorder(q->right);
+                
+    printf("%d ",q->data);
+                
+            
+  }
+            
+            
+            
+        
+}
+        
+int main()
+        
+{
+            
+  struct bt *root=(struct bt*)malloc(sizeof(struct bt));
+            
+  root->data=-1;
+            
+  insert(root,3,'l');
+            
+  insert(root,2,'r');
+           
+  insert(root->left,5,'l');
+            
+  insert(root->right,7,'l');
+            
+  printf("\n display element in inorder:- ");
+            
+  inorder(root);
+            
+  printf("\n display element in preorder:- ");
+           
+  preorder(root);
+            
+  printf("\n display element in postorder:- ");
+            
+  postorder(root);
+            
+  return 1;
+            
+        
+}
+Output:
+display element in inorder:- 5 3 -1 7 2 
+display element in preorder:- -1 3 5 2 7 
+display element in postorder:- 5 3 7 2 -1 
