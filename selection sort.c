0d0e1e75568da04d01fb5bbce702a407265d1cf3
sushmabalikai <94390036+sushmabalikai@users.noreply.github.com>
2021-12-27 19:38:38 +0530

    Add files via upload

---
+++ selection sort.c
@@ -0,0 +1,71 @@
+/*Selection Sort Algorithm*/
+#include <stdio.h>
+
+void selectionsort(int arr[],int size)
+
+{
+   
+ for(int j=0;j<size-1;j++)
+    
+{
+        
+  int min=j;
+       
+  for(int i=j+1;i<size;i++)
+      
+  {
+           
+    if(arr[i]<arr[min])
+          
+    min=i;
+       
+ }
+       
+ int temp=arr[j];
+        
+ arr[j]=arr[min];
+        
+ arr[min]=temp;
+   
+ }
+
+}
+
+void print_Array(int arr[],int len)
+
+{
+    
+   int i;
+    
+   for(i=0;i<len;i++)
+    
+   printf("%d ",arr[i]);
+
+}
+
+int main()
+
+{
+    
+  int array[]={14,17,8,90,11,2};
+   
+  int length=sizeof(array)/sizeof(array[0]);
+    
+  printf("Before sorting the array:");
+   
+  print_Array(array,length);
+    
+  printf("\n");
+   
+  printf("After sorting the array:");
+   
+  selectionsort(array,length);
+    
+  print_Array(array,length);
+    
+ return 0;
+
+} 
+Output:
+Before sorting the array:14 17 8 90 11 2 
+After sorting the array:2 8 11 14 17 90 
